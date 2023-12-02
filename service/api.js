c017ea4aa35b27c2440d8b7661e37af9386eb93f
ji25411998001 <ji25411998001@gmail.com>
2023-12-02 20:59:14 +0700

    ok

--- service/api.js
+++ service/api.js
@@ -74,8 +74,8 @@ router.get('/test', (req, res) => {
     let data = {
         "key": "lJDqwAAcnoZJo3k6boLhLTojo1E8p3gtLbapYnibSuqoEV9LFg3IgZsl5Eoq",
         "model_id": "dark-sushi-mix-mix",
-        "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
-        "negative_prompt": "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime",
+        "prompt": "cat",
+        "negative_prompt": "",
         "width": "512",
         "height": "512",
         "samples": "1",

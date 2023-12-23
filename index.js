5cb72bd3bc6656f52fbc439d18338f427defee9d
ji25411998001 <ji25411998001@gmail.com>
2023-12-23 14:50:12 +0700

    update

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(5000, () => {
